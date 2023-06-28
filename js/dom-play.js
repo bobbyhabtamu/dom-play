779c101a253edb2d06e345bfd38da2bd38b47b70
Bobby Habtamu <bobby.habtamu@gmail.com>
2023-06-28 11:29:01 -0700

    Adds Span

---
+++ js/dom-play.js
@@ -0,0 +1,18 @@
+alert("Hi from DOM Play!");
+
+
+/*
+
+    Scenario: Create an app that on click, actors lines are highlighted
+
+    1. With a single click, highlight (bold or otherwise) all lines for a specific actor in a play
+    Un-highlight (normal or otherwise) all lines from all other actors in the play
+    
+    2. The function(s) created to do the work must accommodate any number of actors, and any number of additional lines added to the page
+    
+    3. HTML "hooks" may be added to the page for proper operation
+
+*/
+
+/* document.querySelector(".hamlet").style.backgroundColor = "yellow";
+*/
